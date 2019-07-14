9e3371ecccaff1dcd3ec8f341c74b18bad6f6dba
zeit <zei.t@qq.com>
2019-07-15 01:34:24 +0800

    add • save image with specific name

--- DEMO_gray.m
+++ DEMO_gray.m
@@ -5,17 +5,18 @@
 %% Initialize parameters
 clear; close all; clc;
 UPSAMP.factor = 4;  % magnification factor
-UPSAMP.iter = 4;  % times of iteration
-DECONV.lambda_1 = 0.01;  % parameters
-DECONV.lambda_2 = 20;
-DECONV.iter_max =10;  % max iteration times...
-%                       for deconvolution
-GAU.size = 13;  % size of gaussian kernel
-GAU.var = 1.05;  % variance of gaussian kernel
+UPSAMP.iter = 3;  % times of iteration
+DECONV.lambda_1 = 0.01;  % lambda_1
+DECONV.lambda_2 = 1;  % lambda_2
+DECONV.iter_max =10;  % max deconv iterations
+GAU.size = 11;  % size of gaussian kernel
+GAU.var = 1;  % variance of gaussian kernel
+IMG.name = 'words';
+IMG.read = ['images/', IMG.name, '_small.jpg'];
 
 %% Load original image and split channels
 % Stuff about image L (input low resolution image).
-L = im2double(imread('images/bw_200x200.png'));
+L = im2double(imread(IMG.read));
 L = rgb2gray(L);
 L = imbinarize(L);
 
@@ -36,22 +37,26 @@ for i = 1:UPSAMP.iter
     %¡¾Normalize¡¿
     [H_tilde_norm, low, gap] = simpnormimg(H_tilde);
     
-%     %¡¾Non-blind deconvolution¡¿
-%     H_star = nbDeconv(H_tilde_norm, DECONV, GAU);
+    %¡¾Non-blind deconvolution¡¿
+    H_star = nbDeconv(H_tilde_norm, DECONV, GAU);
+    IMG.appro = 'mine';
+    IMG.other = [num2str(DECONV.lambda_2), '_lam2'];
 
-    %¡¾Lagrangian approach¡¿
-    kernel = fspecial('gaussian', GAU.size, GAU.var);
-    H_star = fftCGSRaL(H_tilde_norm, kernel);
+%     %¡¾Non-blind deconvolution: Lagrangian approach¡¿
+%     kernel = fspecial('gaussian', GAU.size, GAU.var);
+%     H_star = fftCGSRaL(H_tilde_norm, kernel);
+%     IMG.appro = 'caip';
+%     IMG.other = '';
+
+    %¡¾Print and denormalize¡¿
     disp([newline,' ============>¡¾Iteration ',...
     num2str(i), ' done!¡¿',newline]);
-
-    %¡¾Denormalize¡¿
     H_star = H_star*gap + low;
     
     if(i == UPSAMP.iter),break;end
     
     %¡¾Reconvolution¡¿
-    PSF = fspecial('gaussian', GAU.size, GAU.var);
+    PSF = fspecial('gaussian', 3, 1);
     H_s = imfilter(H_star, PSF, 'same', 'conv');
     
     %¡¾Pixel substitution¡¿
@@ -60,15 +65,27 @@ for i = 1:UPSAMP.iter
             1:UPSAMP.factor:end) = L(:,:);
 end
 
-%% Show result
+%% Show and save result
 FIG_HANDLE.H = figure('Name', 'IMG - High Resolution');
 movegui(FIG_HANDLE.H, 'east');
 figure(FIG_HANDLE.H);
 imshow(H_star);
 title(['After ', num2str(i), ' iterations']);
 
+mkdir(['results/', IMG.name]);
+IMG.write = ['results/', IMG.name, '/',...
+    [IMG.name, '_', IMG.appro,],...
+    ['_', num2str(UPSAMP.factor), 'x'],...
+    ['_', num2str(UPSAMP.iter), '_iter'],...
+    ['_', num2str(GAU.size),'-', num2str(GAU.var), '_gau'],...
+    ['_', IMG.other],...
+    '.jpg'];
+imwrite(H_star, IMG.write);
+disp([newline,' ============>¡¾Image "',...
+    IMG.write, '" saved!¡¿',newline]);
+
 %% Function on call
-function [I low gap] = simpnormimg(G)
+function [I, low, gap] = simpnormimg(G)
 
 lb = min(G(:));
 ub = max(G(:));
